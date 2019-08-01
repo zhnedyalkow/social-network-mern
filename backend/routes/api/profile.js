2b183bd76682676f77330851bf9d41c5d53bae91
Zhitomir Oreshenski <zhitomir.oreshenski@mentormate.com>
2019-08-01 15:06:01 +0300

    route /profile get/post profile with validations

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,11 +1,118 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route GET api/profile/test
-// @desc Test profile route
-// @access Public
+mongoose.set('useFindAndModify', false);
+
+// Load validation
+const validateProfileInput = require('../../validation/profile');
+
+// Load Profile Model
+const profile = require('../../models/Profile');
+
+// Load User Model
+const user = require('../../models/User');
+
+// @route GET api/profile
+// @desc Get current users profile
+// @access Private
+
+// use passport for private (protected route)
+router.get('/', passport.authenticate('jwt', {
+	session: false
+}), (req, res) => {
+	const errors = {};
+
+	Profile.findOne({
+			user: req.user.id
+        })
+        .populate('user', ['name', 'avatar'])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = 'There is no profile for this user';
+				return res.status(404).json(errors);
+			}
+			res.json(profile);
+		})
+		.catch(err => res.status(404).json(err));
+})
 
 
-router.get('/test', (req, res) => res.json({msg: 'Profile works'}));
+// @route POST api/profile
+// @desc Create or edit user profile
+// @access Private
+
+router.post('/', passport.authenticate('jwt', {
+	session: false
+}), (req, res) => {
+	// Get fields
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation
+    if(!isValid) {
+        // return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+	const profileFields = {};
+	profileFields.user = req.user.id;
+
+	// Check to see if fields are sent by the form
+	if (req.body.handle) profileFields.handle = req.body.handle;
+	if (req.body.company) profileFields.company = req.body.company;
+	if (req.body.website) profileFields.website = req.body.website;
+	if (req.body.location) profileFields.location = req.body.location;
+	if (req.body.status) profileFields.status = req.body.status;
+	if (req.body.bio) profileFields.bio = req.body.bio;
+	if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+
+	// Skills - Split into array - it comes as comma separated values
+	if (typeof req.body.skills !== 'undefined') profileFields.skills = req.body.skills.split(',');
+
+	// Social
+	profileFields.social = {};
+	if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+	if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+	if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+	if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+	if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			if (profile) {
+                // console.log(`/api/profile: profile? => ${profile}`);
+			// Update
+			  Profile.findOneAndUpdate(
+				{ user: req.user.id },
+				{ $set: profileFields },
+				{ new: true }
+              )
+              .then(profile => res.json(profile))
+
+			} else {
+            // Create
+              
+             // Check if handle exist
+            //  console.log(`create user`);
+            //  console.log(`/api/profile: profile? => ${profile}`);
+
+              Profile.findOne({ handle: profileFields.handle })
+              .then(profile => {
+                  if(profile) {
+                      errors.handle = 'That handle already exists';
+                      res.status(400).json(errors);
+                  }
+
+                //   console.log(`save profile`);
+                  // Save Profile
+                  new Profile(profileFields)
+                  .save()
+                    .then(profile => res.json(profile));
+
+                    // console.log(`save profile`);
+              })
+			}
+		})
+})
 
 module.exports = router;
