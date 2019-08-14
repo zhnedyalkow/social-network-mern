a7b42ea214549687bf4cac332eabbf2a033be189
zh.nedyalkow <zh.nedyalkow@gmail.com>
2019-08-14 11:13:25 +0300

    Register/Login validation & state

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>  
-    </div>
-  );
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={ Landing } />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Landing />
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
