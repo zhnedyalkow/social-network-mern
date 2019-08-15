9125595bc95b80785ac35b3c11f0d15983d4884e
zh.nedyalkow <zh.nedyalkow@gmail.com>
2019-08-15 10:01:30 +0300

    Setup store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -9,10 +11,12 @@ import Login from './components/auth/Login';
 
 import "./App.css";
 
+
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Provider store={store}>
+        <Router>
         <div className="App">
           <Navbar />
           <Route exact path="/" component={ Landing } />
@@ -24,18 +28,9 @@ class App extends Component {
           <Footer />
         </div>
       </Router>
+      </Provider>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Landing />
-//       <Footer />
-//     </div>
-//   );
-// }
 
 export default App;
